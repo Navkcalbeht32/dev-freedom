3f67ebb687ee6642d11d5eab9b2c50427ae2b6a0
Navkcalbeht32 <asumoski@gmail.com>
2021-01-15 23:39:36 -0500

    card grid responsive, component built, image, heading, title

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import CTA from './components/CTA/CTA.component';
 import Footer from './components/Footer/Footer.component';
 import Banner from './components/Banner/Banner.component';
 import CustomCards from './components/CustomCards/CustomCards.component';
+import Data from './assets/data.jpg';
+import Team from './assets/team.jpg';
+import Shop from './assets/shop.jpg';
 
 function App() {
   return (
@@ -17,8 +20,17 @@ function App() {
           <Banner />
           <Navbar />
           <Hero />
-          <Grid container direction="row">
-            <CustomCards />
+          <Grid container direction="row" className="card-grid">
+            <Grid item xs={12} sm={6} md={4}>
+              <CustomCards title="Guide" heading="Erasing Your Digital Footprint: A Guide" image={Data}/>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <CustomCards title="title" heading="Development: Infrastructure of the Future" image={Team}/>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <CustomCards title="title" heading="Security in Mind Products: Tech & More" image={Shop} />
+            </Grid>
+            
           </Grid>
           <Grid container direction="row">
             <CTA />
--- src/components/CustomCards/CustomCards.component.jsx
+++ src/components/CustomCards/CustomCards.component.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './CustomCards.styles.scss';
 
-const CustomCards = () => {
+
+const CustomCards = ({image, title, heading}) => {
     return (
         <div className="custom-cards">
-
+            <img src={image} alt="" className="card-image"/>
+            <div className="card-info">
+                <h4>{title}</h4>
+                <h3>{heading}</h3>
+            </div>
         </div>
     )
 }
